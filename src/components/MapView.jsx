447ddd1b54cc3f5b5f766e799f266207f451ebba
Rafly <rafly.setia.2405366@students.um.ac.id>
2026-07-01 15:24:30 +0700

    ok

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,14 +1,9 @@
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import "./MapView.css";
 
 const center = {
-  lat: -7.8875,
-  lng: 112.6025,
-};
-
-const containerStyle = {
-  width: "100%",
-  height: "70vh",
-  borderRadius: "20px",
+  lat: -7.8847,
+  lng: 112.5746,
 };
 
 function MapView() {
@@ -16,34 +11,40 @@ function MapView() {
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
 
-  if (!isLoaded)
+  if (!isLoaded) {
     return (
-      <div
-        style={{
-          height: "70vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white",
-        }}
-      >
-        Memuat Google Maps...
+      <div className="map-wrapper">
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            fontSize: "20px",
+          }}
+        >
+          Memuat Google Maps...
+        </div>
       </div>
     );
+  }
 
   return (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={15}
-      options={{
-        streetViewControl: false,
-        mapTypeControl: true,
-        fullscreenControl: true,
-      }}
-    >
-      <Marker position={center} />
-    </GoogleMap>
+    <div className="map-wrapper">
+      <GoogleMap
+        mapContainerClassName="google-map"
+        center={center}
+        zoom={15}
+        mapTypeId="satellite"
+        options={{
+          fullscreenControl: true,
+          streetViewControl: false,
+          mapTypeControl: true,
+          zoomControl: true,
+        }}
+      />
+    </div>
   );
 }
 
