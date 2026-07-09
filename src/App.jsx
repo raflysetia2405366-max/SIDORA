88c3579f505445bf25f93b8830259583f2dfd829
Rafly <rafly.setia.2405366@students.um.ac.id>
2026-07-09 21:53:45 +0700

    k

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
-import Dashboard from "./pages/Dashboard";
 import MapPage from "./pages/MapPage";
-import Profile from "./pages/Profile";
-import Potential from "./pages/Potential";
-import Service from "./pages/Service";
-import About from "./pages/About";
 
 import "./App.css";
 
@@ -23,40 +17,9 @@ function App() {
         <main className="main-content">
 
           <Routes>
-
-            <Route
-              path="/"
-              element={<Dashboard />}
-            />
-
-            <Route
-              path="/dashboard"
-              element={<Dashboard />}
-            />
-
             <Route
               path="/map"
               element={<MapPage />}
-            />
-
-            <Route
-              path="/profile"
-              element={<Profile />}
-            />
-
-            <Route
-              path="/potential"
-              element={<Potential />}
-            />
-
-            <Route
-              path="/service"
-              element={<Service />}
-            />
-
-            <Route
-              path="/about"
-              element={<About />}
             />
 
           </Routes>
